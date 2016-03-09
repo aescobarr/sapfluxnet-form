9014595656452c79e1acca7829b5606e3fe5c4f2
aescobarr <agustiescobar@gmail.com>
2016-03-09 15:05:21 +0100

    Clean up removed old comments

--- sfnform/views.py
+++ sfnform/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
-#from models import MetadataFormForm
 from forms import MetadataFormForm
 from datetime import datetime
 
