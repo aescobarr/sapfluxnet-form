abee30b8040bbf45f442e075cd42414f54dbd293
aescobarr <agustiescobar@gmail.com>
2016-03-09 14:39:19 +0100

    Env and wsgi scripts

--- sapfluxnet/wsgi.py
+++ sapfluxnet/wsgi.py
@@ -7,9 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-import os
+import os,sys
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append('/data/webapps/sapfluxnet-form')
+
+sys.path.append('/data/webapps/sapfluxnet-form/sfnenv/lib/python2.7/site-packages')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sapfluxnet.settings")
 
