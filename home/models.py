7821f79cfd6107a2bb5e9a9d64bcad5883d9ae6f
MalditoBarbudo <victorgrandagarcia@gmail.com>
2016-04-13 18:59:12 +0200

    improved header and footer

--- home/models.py
+++ home/models.py
@@ -8,9 +8,12 @@ class ShinyApp(models.Model):
     shiny_name = models.CharField(max_length=200)
     shiny_description = models.CharField(max_length=200, default='')
     shiny_show = models.BooleanField(default=True)
+    shiny_address = models.URLField(max_length=200, default='')
     def __str__(self):
         return self.shiny_name
     def is_showed(self):
         return self.shiny_show
     def show_desc(self):
         return self.shiny_description
+    def show_url(self):
+        return self.shiny_address
